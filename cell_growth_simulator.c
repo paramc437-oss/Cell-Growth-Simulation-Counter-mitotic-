f9ebb84758430dd4b358982275eb1b1e55fcfd3c
paramc437-oss <paramc437@gmail.com>
2026-06-16 08:10:30 +0000

    Add files via upload

---
+++ cell_growth_simulator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int cells;
+    int days;
+    printf("welcome to our first cell growth simulation project in which we have implied basic knowledge and functions of c language\n");
+    printf("note that this is only for mitotic assuming that cells double every day of input\n");
+    printf("what is the day of your cell replication");
+    scanf("%d", &days);
+    printf("what is your current cell population ");
+    scanf("%d", &cells);
+    printf("based on your input your number of cells are %d", cells * 2);
+}
